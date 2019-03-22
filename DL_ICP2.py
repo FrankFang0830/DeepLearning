bd939e2ad29d0d1a4adbca3680252bc42a6de1ec
FrankFang0830 <44310469+FrankFang0830@users.noreply.github.com>
2019-03-22 17:56:15 -0500

    none

---
+++ DL_ICP2.py
@@ -0,0 +1,71 @@
+from keras import Sequential
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.layers import Dense
+from keras.utils import to_categorical
+
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='1'
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3'
+
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+(train_images,train_labels),(test_images, test_labels) = mnist.load_data()
+#display the first image in the training data
+plt.imshow(train_images[0,:,:],cmap='gray')
+plt.title('Ground Truth : {}'.format(train_labels[0]))
+plt.show()
+
+#process the data
+#1. convert each image of shape 28*28 to 784 dimensional which will be fed to the network as a single feature
+dimData = np.prod(train_images.shape[1:])
+
+train_data = train_images.reshape(train_images.shape[0],dimData)
+test_data = test_images.reshape(test_images.shape[0],dimData)
+
+#convert data to float and scale values between 0 and 1
+train_data = train_data.astype('float')
+test_data = test_data.astype('float')
+#scale data
+train_data /=255.0
+test_data /=255.0
+#change the labels frominteger to one-hot encoding
+train_labels_one_hot = to_categorical(train_labels)
+test_labels_one_hot = to_categorical(test_labels)
+
+#creating network
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(dimData,)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(train_data, train_labels_one_hot, batch_size=256, epochs=20, verbose=1,
+                   validation_data=(test_data, test_labels_one_hot))
+
+[test_loss, test_acc] = model.evaluate(test_data, test_labels_one_hot)
+print("Evaluation result on Test Data : Loss = {}, accuracy = {}".format(test_loss, test_acc))
+
+#q1
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.ylabel('val_acc')
+plt.xlabel('acc')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+#q2
+plt.imshow(test_images[1])
+plt.show()
+t1=test_images[1]
+test_data1 = t1.reshape(1, 784)
+labels=model.predict(test_data1)
+print(labels)
+
